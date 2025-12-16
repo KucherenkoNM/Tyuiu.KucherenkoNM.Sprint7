2c82798597490e99cb1755ef41eb6951d140a204
Надежда Кучеренко <Tyuiu.kucherenkonm@outlook.com>
2025-12-16 19:44:41 +0500

    Add project files.

---
+++ Tyuiu.KucherenkoNM.Sprint7.Project.V12.Lib/DataService.cs
@@ -0,0 +1,7 @@
+﻿namespace Tyuiu.KucherenkoNM.Sprint7.Project.V12.Lib
+{
+    public class DataService
+    {
+
+    }
+}
---
+++ Tyuiu.KucherenkoNM.Sprint7.Project.V12/FormMain.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.KucherenkoNM.Sprint7.Project.V12
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
