b66f61f3154e632a1a384f0504bd5a69c2c06969
Надежда Кучеренко <Tyuiu.kucherenkonm@outlook.com>
2025-12-19 00:54:10 +0500

    Lib

--- Tyuiu.KucherenkoNM.Sprint7.Project.V12.Lib/Models/Computer.cs
+++ Tyuiu.KucherenkoNM.Sprint7.Project.V12.Lib/Models/Computer.cs
@@ -4,13 +4,13 @@ namespace Tyuiu.KucherenkoNM.Sprint7.Project.V12.Lib.Models
 {
     public class Computer
     {
-        public int ComputerId { get; set; }       // идентификатор ПК
-        public string EvmType { get; set; }        // тип ЭВМ
-        public int ManufacturerId { get; set; }    // связь с производителем
-        public int RetailerId { get; set; }        // связь с реализатором
-        public int ProcessorId { get; set; }       // связь с процессором
-        public string StorageType { get; set; }    // тип накопителя
-        public DateTime ReleaseDate { get; set; }  // дата выпуска
-        public decimal Price { get; set; }         // цена
+        public int ComputerId { get; set; }
+        public string EvmType { get; set; } 
+        public int ManufacturerId { get; set; } 
+        public int RetailerId { get; set; }
+        public int ProcessorId { get; set; }
+        public string StorageType { get; set; } 
+        public DateTime ReleaseDate { get; set; } 
+        public decimal Price { get; set; }  
     }
 }
